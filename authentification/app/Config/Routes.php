6c97b637740644b581b9bd855fa068587d92f03d
Bilal-EZ-ZAIM <bilalzaim499@gmail.com>
2024-05-03 12:34:50 +0100

    j'ai terminé le project

--- authentification/app/Config/Routes.php
+++ authentification/app/Config/Routes.php
@@ -9,3 +9,4 @@ $routes->get('/', 'Home::index');
 $routes->get('/register', 'Utilisateurs::getRegister');
 $routes->get('/login', 'Utilisateurs::getLogin');
 $routes->post('/auth/register', 'Utilisateurs::register');
+$routes->post('/auth/login', 'Utilisateurs::login');
